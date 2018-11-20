8a1fea77da9b8b814aa741c0a33396159ccb0523
joe <zdnf_lgq@163.com>
2018-11-21 01:38:44 +0800

    更新左侧二级菜单显示

--- taobao/index.js
+++ taobao/index.js
@@ -1,26 +1,55 @@
 var index = 0;
 init();
-function init(){
+function init() {
 	$('#pic-box a').eq(index).fadeIn(300).siblings('a').fadeOut(300);
-	$('.list li').eq(index).css('background','#f40').siblings('li').css('background','#ddd');
+	$('.list li').eq(index).css('background', '#f40').siblings('li').css('background', '#ddd');
 }
 
-function run(){
+function run() {
 	index++;
 	if (index == 5) {
 		index = 0;
 	}
 	init();
 }
-var timer = setInterval(run,1800);
-$('#pic-box').hover(function(){
+var timer = setInterval(run, 1800);
+$('#pic-box').hover(function () {
 	if (timer) {
 		clearInterval(timer);
 	}
-},function(){
-	timer = setInterval(run,1800);
+}, function () {
+	timer = setInterval(run, 1800);
 })
-$('.list li').mouseenter(function(){
+$('.list li').mouseenter(function () {
 	index = $(this).index();
 	init();
 })
+
+// 二级菜单选项
+var on = false;
+var selOn = false;
+var sel = document.getElementsByClassName('inner-con-sel')[0];
+var selLis = sel.getElementsByTagName('li');
+var innerLeft = document.getElementById('inner-left-ul');
+var lis = innerLeft.getElementsByTagName('li');
+
+
+for (var i = 0; i < lis.length; i++) {
+	(function (i) {
+		lis[i].onmouseenter = function () {
+			selLis[i].style.display = 'block';
+			// on = true
+		}
+		lis[i].onmouseleave = function () {
+				selLis[i].style.display = 'none'
+		}
+		selLis[i].onmouseenter = function(){
+			selLis[i].style.display = 'block'
+		}
+		selLis[i].onmouseleave = function () {
+			selLis[i].style.display = 'none'
+		}
+
+	})(i)
+}
+
