52315d6d66e90f3c8233998f424b308ab07b8a28
joe <zdnf_lgq@163.com>
2018-11-22 00:44:18 +0800

    更新轮播图

--- taobao/index.js
+++ taobao/index.js
@@ -1,29 +1,43 @@
-var index = 0;
-init();
-function init() {
-	$('#pic-box a').eq(index).fadeIn(300).siblings('a').fadeOut(300);
-	$('.list li').eq(index).css('background', '#f40').siblings('li').css('background', '#ddd');
-}
+// 轮播图结构:凡客样式
+// var index = 0;
+// init();
+// function init() {
+// 	$('#pic-box a').eq(index).fadeIn(300).siblings('a').fadeOut(300);
+// 	$('.list li').eq(index).css('background', '#f40').siblings('li').css('background', '#ddd');
+// }
 
-function run() {
-	index++;
-	if (index == 5) {
-		index = 0;
-	}
-	init();
-}
-var timer = setInterval(run, 1800);
-$('#pic-box').hover(function () {
-	if (timer) {
-		clearInterval(timer);
-	}
-}, function () {
-	timer = setInterval(run, 1800);
+// function run() {
+// 	index++;
+// 	if (index == 5) {
+// 		index = 0;
+// 	}
+// 	init();
+// }
+// var timer = setInterval(run, 1800);
+// $('#pic-box').hover(function () {
+// 	if (timer) {
+// 		clearInterval(timer);
+// 	}
+// },
+// function () {
+// 		timer = setInterval(run, 1800);
+// 	})
+// $('.list li').mouseenter(function () {
+// 	index = $(this).index();
+// 	init();
+// })
+
+// 轮播图：淘宝样式
+$('#carousel_1').FtCarousel({
+	index:0,
+	time:2500
+});
+$('#carousel_1').mouseenter(()=>{
+	$('.carousel-btn').css('display','block')
+}).mouseleave(()=>{
+	$('.carousel-btn').css('display','none')
 })
-$('.list li').mouseenter(function () {
-	index = $(this).index();
-	init();
-})
+
 
 // 二级菜单选项
 var sel = document.getElementsByClassName('inner-con-sel')[0];
@@ -37,13 +51,13 @@ for (var i = 0; i < lis.length; i++) {
 		lis[i].onmouseenter = () => {
 			selLis[i].style.display = 'block'
 		}
-		lis[i].onmouseleave = () =>{
+		lis[i].onmouseleave = () => {
 			selLis[i].style.display = 'none'
 		}
-		selLis[i].onmouseenter = () =>{
+		selLis[i].onmouseenter = () => {
 			selLis[i].style.display = 'block'
 		}
-		selLis[i].onmouseleave = () =>{
+		selLis[i].onmouseleave = () => {
 			selLis[i].style.display = 'none'
 		}
 
@@ -51,6 +65,6 @@ for (var i = 0; i < lis.length; i++) {
 }
 
 // 取消所有 a标签的默认跳转事件
-$('a').click(()=>{
+$('a').click(() => {
 	return false;
 })
